02cd956abbc064f7a64910fba2b5673694b704bf
Nikita Madorsky <madorskii@mail.ru>
2023-08-26 08:53:53 +0300

    added index in db

--- init.sql
+++ init.sql
@@ -3,16 +3,17 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS segments (
-                                        segment_id serial PRIMARY KEY,
-                                        slug varchar(255) NOT NULL UNIQUE
-    );
+    segment_id serial PRIMARY KEY,
+    slug varchar(255) NOT NULL UNIQUE
+);
 
 CREATE TABLE user_segments (
-                               user_id INT,
-                               segment_id INT,
-                               FOREIGN KEY (user_id) REFERENCES users(user_id),
-                               FOREIGN KEY (segment_id) REFERENCES segments(segment_id),
-                               PRIMARY KEY (user_id, segment_id)
+   user_id INT,
+   segment_id INT,
+   FOREIGN KEY (user_id) REFERENCES users(user_id),
+   FOREIGN KEY (segment_id) REFERENCES segments(segment_id),
+   PRIMARY KEY (user_id, segment_id)
 );
 
 INSERT INTO users SELECT generate_series(1, 100);
+CREATE INDEX user_segments_user_id_idx ON user_segments (user_id);
